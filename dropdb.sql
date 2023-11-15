98fb89aedbaeb483307be48ac9619456150fee2a
Somin Ji <86943370+ji-somnie@users.noreply.github.com>
2023-11-16 00:47:47 +0900

    Add files via upload

---
+++ dropdb.sql
@@ -0,0 +1,8 @@
+use db2022team02;
+drop table DB2022_리뷰; 
+drop table DB2022_배달;
+drop table DB2022_매출;
+drop table DB2022_고객정보;
+drop table DB2022_메뉴;
+drop table DB2022_식당;
+drop database db2022team02;
